b934a876b2ec6c2f1f8572a0976b3db7998f733f
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-04-01 19:15:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+//          100 days of coding challenge ( DAY 18 )
+Object.defineProperty(exports, "__esModule", { value: true });
+//   Question 01   ---Advertising a mobile phone
+let smartphone = {
+    make: "Infinix",
+    model: "S5 lite",
+    specs: {
+        Storage: "64GB",
+        ScreenSize: "7.6",
+        batteryLife: "1 day bakeup",
+    }
+};
+// shows what features my mobile have.
+console.log(smartphone);
+//   Question 02    
+let developersSkills = {
+    languages: ["Javascript", "Python", "C++"],
+    frameworks: ["React", "Angular", "Vue"],
+    tools: ["Git", "Webpc", "Docker"],
+};
+// --Getting more skills from this list
+let { languages, frameworks, tools } = developersSkills;
+//  -- show a skill
+console.log(`languages: ${languages[0]},frameworks: ${frameworks[0]}, tools: ${tools[0]}`);
+console.log(`languages: ${languages[1]},frameworks: ${frameworks[1]}, tools: ${tools[1]}`);
+console.log(`languages: ${languages[2]},frameworks: ${frameworks[2]}, tools: ${tools[2]}`);
+//   Question 03 
+//  --A way to make a flexible list
+function createObjectWithDynamicKey(key, value) {
+    let dynamicObject = [{}];
+    // --Setting up a section in the list with a changeable name
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+// --Using the flexible list setup for a user's preference
+let userPreference = createObjectWithDynamicKey("theme", "dark");
+// Showing the user's choice
+console.log(userPreference);
